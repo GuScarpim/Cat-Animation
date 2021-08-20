523edf629a14f986add15b2654fd13393bf8a9f3
guscarpim <gustavoscarpim@gmail.com>
2021-08-20 01:12:20 -0300

    Projeto React

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Cat from './Components/Cat/index'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Cat />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
